372aa9a14c72ead9959c1268dd44e1b08d675c94
ledmington <filippo.barbari@gmail.com>
2022-09-15 16:35:24 +0200

    Setup workflows

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
 import './assets/main.css'
 
 createApp(App).mount('#app')
+
+while (true) {
+    axios.get("localhost:3000").then(response => console.log(response.data));
+}
