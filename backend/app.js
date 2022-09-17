55cb7c6bae95dd0f374e973d91b0c511ca44d0bc
Ledmington <ledmington.dev@gmail.com>
2022-09-17 15:36:09 +0200

    wip

--- backend/app.js
+++ backend/app.js
@@ -11,4 +11,8 @@ app.get('/', (req, res) => {
   console.log('Received a GET on /');
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
